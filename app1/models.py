7e82821939a9946af00bcf22cee7f2cd5ac2b236
brito101 <rodrigobrito101@hotmail.com>
2023-08-14 19:07:43 -0300

    first commit

---
+++ app1/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField("name", max_length=30)
+    phone = models.IntegerField("phone")
+    email = models.EmailField("email", max_length=30)
+
+    def __str__(self):
+        return f"Nome: {self.name}, telefone: {self.phone}, email: {self.email}"
